5efbd4ec1b3b8f4bb0576f5279844acd05cd2139
taroq4-2 <taroq4@gmail.com>
2026-06-20 15:56:06 +0300

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,18 @@
-plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.4")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
